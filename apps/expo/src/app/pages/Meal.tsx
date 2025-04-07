8fb81650f4d3b717d36bf3399c859b91e70fbe9c
therealdanmalmx <danmalmx@gmail.com>
2025-04-07 21:13:24 +0200

    Update image cnad confirm text width

--- apps/expo/src/app/pages/Meal.tsx
+++ apps/expo/src/app/pages/Meal.tsx
@@ -25,7 +25,7 @@ const Meal = () => {
     const mealImages: Record<string, any> = {
         'vegan': require("../../../assets/images/vegan-meal.jpg"),
         'vegetarian': require("../../../assets/images/vegetarian-meal.jpg"),
-        'flexitarian': require("../../../assets/images/flexitarian-meal.png"),
+        'flexitarian': require("../../../assets/images/flexitarian-meal.jpg"),
      };
 
 
@@ -33,9 +33,9 @@ const Meal = () => {
     <View className='meals'>
         <SafeAreaView className="h-full" edges={["top", "bottom"]}>
             <View className='w-full'>
-                <ProgressBar progression={totalMeals} numProgressions={3} points={50}/>
+                <ProgressBar progression={totalMeals} numProgressions={3} points={50} />
             </View>
-            {!confirm ? (
+            {!confirm && (
                 <>
                     <View className='mt-8'>
                         <Text className="text-[20px] font-semibold sora py-5">Select the type of meal</Text>
@@ -45,7 +45,7 @@ const Meal = () => {
                             <Pressable
                                 key={mealType.id}
                                 onPress={() => handleMealSelect(mealType.id)}
-                                className={`meals-card ${selectedMealId !== mealType.id ? 'bg-white' : 'green-bg-50'} w-full h-full border border-[#D9EBB2]`}
+                                className={`meals-card ${selectedMealId !== mealType.id ? 'bg-white' : 'green-bg-50'}`}
                             >
                                 <View className={`meals-radio ${selectedMealId !== mealType.id ? 'border border-[#E7E5E4]' : 'border-[2px] border-[#A1CE3F]'}`} />
                                 <Image resizeMode="contain" source={mealType.icon} className="size-[100px]"/>
@@ -61,11 +61,11 @@ const Meal = () => {
                         </TouchableOpacity>
                     </View>
                 </>
-
-            ): (
+            )}
+            {confirm && (
                 <>
-                    <Image resizeMode="center" source={selectedMealId ? mealImages[selectedMealId] : undefined} />
-                    <View className='mx-auto w-1/2 -mt-36'>
+                    <Image resizeMode="center" source={selectedMealId ? mealImages[selectedMealId] : undefined} className='w-full'/>
+                    <View className='mx-auto w-10/12 -mt-36'>
                         {selectedMealId === 'vegan' && (
                             <Text className='text-center'>Eating a vegan diet can reduce your carbon footprint by 73%, save water, forests and hundreds of animals a year.</Text>
                         )}
