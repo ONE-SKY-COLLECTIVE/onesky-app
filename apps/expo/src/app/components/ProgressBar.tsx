f79b2fbf538e6392b338caee278e8f9ab782abe2
Stephen Shin <stephenshin@Stephens-Laptop.local>
2025-05-21 17:22:45 -0700

    progressbar update

--- apps/expo/src/app/components/ProgressBar.tsx
+++ apps/expo/src/app/components/ProgressBar.tsx
@@ -6,36 +6,45 @@ interface ProgressBarProps {
     numProgressions: number;
     points: number;
     utility?: () => void;
+    title?: string;
 }
-const ProgressBar: React.FC<ProgressBarProps> = ({progression, points, numProgressions, utility}) => {
+const ProgressBar: React.FC<ProgressBarProps> = ({progression, points, numProgressions, utility, title}) => {
     const router = useRouter();
-    const progressWidth = 230;
-    const spacing = 10;
-    const numDots = numProgressions;
-    const dotWidth = (progressWidth - (spacing * (numDots - 1))) / numDots;
+    const dotWidth = (100 / numProgressions) - 3
 
     return (
-        <View className="flex items-center justify-between px-5">
-            <TouchableOpacity onPress={utility ? utility : () => router.push("/pages/Homepage")}>
-                <Image resizeMode="contain" source={require("../../../assets/icons/x-button.png")} />
-            </TouchableOpacity>
-            <View className="flex-row items-center" style={{width: progressWidth}}>
+        <View className="flex-col px-5 pb-[20px]">
+            <View className="flex-row items-center align-self-start">
+                <TouchableOpacity className="p-1" onPress={utility ? utility : () => router.push("/pages/Homepage")}>
+                    <Image resizeMode="contain" className="w-[32px] h-[32px]" source={require("../../../assets/icons/x-button.png")} />
+                </TouchableOpacity>
+                {title && 
+                <Text className="sora text-[16px] ml-2 font-semibold">{title}</Text>
+            }
+            </View>
+            <View className="flex-row items-center justify-between w-full mt-[24px]">
                 {Array.from({length: numProgressions}).map((_, index) => (
                     <View 
                         key={index} 
                         style={{
-                            width: dotWidth,
+                            width: `${dotWidth}%`,
                             height: 8,
-                            marginRight: index < numProgressions - 1 ? spacing : 0
                         }}
                         className={`rounded-[999px] ${progression >= index + 1 ? "quiz-progress-active" : "quiz-progress"}`}
-                    />
+                    >
+                        { index + 1 === numProgressions &&
+                            <View className={` top-[15px] flex-row self-end rounded-[16px] border-[#737373] border-[1px] py-[4px] px-[8px] ${progression >= numProgressions && "bg-[#ECF5D9] border-0"}`}>
+                                <Image resizeMode="contain" className="h-[20px] w-[20px] mr-1" source={require("../../../assets/icons/diamond.png")}/>
+                                <Text className={`font-semibold text-[16px] text-[#737373] ${progression >= numProgressions && "text-[#617C26]"}`}>{points}</Text>
+                            </View>
+                        }
+                    </View>
                 ))}
             </View>
-            <View className="flex items-center">
+            {/* <View className="flex items-center">
                 <Image resizeMode="contain" className="diamond-small" source={require("../../../assets/icons/diamond.png")}/>
                 <Text className="raleway ml-1 font-semibold">+ {points}</Text>
-            </View>
+            </View> */}
         </View>
     )
 }
